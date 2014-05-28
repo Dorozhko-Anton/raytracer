e37680236619eefa18e05cfc8261a614c4d4b0b9
Dorozhko Anton <dorozhko.a@gmail.com>
2014-05-29 02:50:41 +0700

    add actions

--- raytracer/src/RenderFrame.java
+++ raytracer/src/RenderFrame.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
  * Created by Anton on 22.05.2014.
  */
 public class RenderFrame {
+    private RenderPanel renderPanel;
     /**
      * Ctor that manages adding menu items, actions, canvas to the mainFrame.
      * May be I should do Init() method instead.
@@ -18,10 +19,82 @@ public class RenderFrame {
     public RenderFrame() {
         final MainFrame mainFrame = new MainFrame("Лабораторная работа №4");
 
+
+
+        AbstractAction leftTurn = new AbstractAction("Поворот налево (a)", null) {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                renderPanel.camera.rotateCamera(-Math.PI / 36, 0, 0);
+                renderPanel.repaintWithRender();
+            }
+        };
+        leftTurn.putValue(AbstractAction.SHORT_DESCRIPTION, "Поворот направо на 5 градусов");
+        mainFrame.addAction(leftTurn);
+        mainFrame.getAboutMenu().add(leftTurn);
+
+        AbstractAction rightTurn = new AbstractAction("Поворот направо (d)", null) {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                renderPanel.camera.rotateCamera(Math.PI / 36, 0, 0);
+                renderPanel.repaintWithRender();
+
+            }
+        };
+        rightTurn.putValue(AbstractAction.SHORT_DESCRIPTION, "Поворот налево на 5 градусов");
+        mainFrame.addAction(rightTurn);
+        mainFrame.getAboutMenu().add(rightTurn);
+
+        AbstractAction forward = new AbstractAction("Вперед (i)", null) {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                renderPanel.camera.moveCamera(new Vector3D(RenderPanel.DR, 0, 0));
+                renderPanel.repaintWithRender();
+            }
+        };
+        forward.putValue(AbstractAction.SHORT_DESCRIPTION, "Вперед!!!");
+        mainFrame.addAction(forward);
+        mainFrame.getAboutMenu().add(forward);
+
+        AbstractAction backward = new AbstractAction("Назад (k)", null) {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                renderPanel.camera.moveCamera(new Vector3D(-RenderPanel.DR, 0, 0));
+                renderPanel.repaintWithRender();
+            }
+        };
+        backward.putValue(AbstractAction.SHORT_DESCRIPTION, "Назад!!!");
+        mainFrame.addAction(backward);
+        mainFrame.getAboutMenu().add(backward);
+
+        AbstractAction stepLeft = new AbstractAction("Шаг влево (j)", null) {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                renderPanel.camera.moveCamera(new Vector3D(0, -RenderPanel.DR, 0));
+                renderPanel.repaintWithRender();
+            }
+        };
+        stepLeft.putValue(AbstractAction.SHORT_DESCRIPTION, "Шаг влево!!!");
+        mainFrame.addAction(stepLeft);
+        mainFrame.getAboutMenu().add(stepLeft);
+
+        AbstractAction stepRight = new AbstractAction("Шаг вправо (l)", null) {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                renderPanel.camera.moveCamera(new Vector3D(0, RenderPanel.DR, 0));
+                renderPanel.repaintWithRender();
+            }
+        };
+        stepRight.putValue(AbstractAction.SHORT_DESCRIPTION, "Шаг вправо!!!");
+        mainFrame.addAction(stepRight);
+        mainFrame.getAboutMenu().add(stepRight);
+
         AbstractAction contacts = new AbstractAction("Контакты", MainFrame.createImageIcon("/images/contacts.png")) {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                JOptionPane.showMessageDialog(mainFrame, "name: Dorozhko Anton \n mail: dorozhko.a@gmail.com\n ");
+                JOptionPane.showMessageDialog(mainFrame, "name: Dorozhko Anton \n mail: dorozhko.a@gmail.com\n " +
+                        "name: Vasilieva Yaroslava \n" +
+                        " mail: --\n" +
+                        " ");
             }
         };
         contacts.putValue(AbstractAction.SHORT_DESCRIPTION, "Как со мной связаться ^_^");
@@ -31,7 +104,7 @@ public class RenderFrame {
         AbstractAction aboutAction = new AbstractAction("О программе", MainFrame.createImageIcon("/images/help.png")) {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                //TODO: fill
+                JOptionPane.showMessageDialog(mainFrame, "Примитивный raytracer");
             }
         };
         aboutAction.putValue(AbstractAction.SHORT_DESCRIPTION, "Кэп сообщает, что это краткое описание задания");
@@ -49,7 +122,7 @@ public class RenderFrame {
 
         mainFrame.getFileMenu().add(exitAction);
 
-        RenderPanel renderPanel = new RenderPanel();
+        renderPanel = new RenderPanel();
         mainFrame.addCanvas(renderPanel);
         mainFrame.setFocusable(true);
 
@@ -60,10 +133,15 @@ public class RenderFrame {
     }
 
     class RenderPanel extends JPanel {
-        private static final double DR = 3;
+        public static final double DR = 3;
         private BufferedImage renderedImage;
         private Scene scene;
         private RenderContext renderContext;
+
+        public Camera getCamera() {
+            return camera;
+        }
+
         private Camera camera;
 
         RenderPanel() {
@@ -138,12 +216,17 @@ public class RenderFrame {
                             camera.moveCamera(new Vector3D(0, 0, -DR));
                             break;
                     }
-                    System.out.println(camera.getWorldPosition());
+                    //System.out.println(camera.getWorldPosition());
                     //renderedImage = Renderer.render(renderContext);
                     renderedImage = ForkJoinRenderer.render(renderContext);
                     repaint();
                 }
             });
+        }
+
+        public void repaintWithRender() {
+            renderedImage = ForkJoinRenderer.render(renderContext);
+            repaint();
         }
 
         public void changeCamerePosition(double x, double y)
